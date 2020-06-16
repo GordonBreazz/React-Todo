fe57230d9540bf4a6402ec15229269539a70d74d
GordonBreazz <jc12-mail@yandex.ru>
2020-06-16 08:44:43 +0800

    props validation

--- src/todo/TodoItem.js
+++ src/todo/TodoItem.js
@@ -1,7 +1,38 @@
-import React from 'react'
+import React from "react"
+import PropTypes from "prop-types"
 
-export default function TodoItem({todo, index}){
+let styles = {
+  li: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: '.5rem 1rem',
+    border: '1px solid #ccc',
+    borderRadius: '4px',
+    marginBottom: '.5rem'
+  },
+  input: {
+    marginRight: '1rem'
+  }
+}
+
+function TodoItem({ todo, index }) {
   return (
-  <li><strong>{index + 1}</strong>{todo.title}</li>
+    <li style={styles.li}>
+      <span>
+        <input type="checkbox" style={styles.input} />
+        <strong>{index + 1}</strong>
+        &nbsp;
+        {todo.title}
+      </span>
+      <button className="rm">&times;</button>
+    </li>
   )
 }
+
+TodoItem.propTypes = {
+  todo: PropTypes.object.isRequired,
+  index: PropTypes.number,
+}
+
+export default TodoItem
--- src/todo/TodoList.js
+++ src/todo/TodoList.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import TodoItem from './TodoItem';
+import TodoItem from './TodoItem'
+import PropTypes from 'prop-types'
 
 const styles ={
   ul: {
@@ -9,14 +10,20 @@ const styles ={
   }  
 }
 
-export default function TodoList(props){
+function TodoList(props){
   return (
     <ul style={styles.ul}>
       {
         props.todos.map( (todo, index) => {
-          return <TodoItem todo={todo} key={todo.id} index={index}/>
+          return <TodoItem todo={todo} key={todo.id} index={index}/> 
         })
       }
     </ul>
   )
 }
+
+TodoList.propTypes = {
+  todos: PropTypes.arrayOf(PropTypes.object).isRequired
+}
+
+export default TodoList
