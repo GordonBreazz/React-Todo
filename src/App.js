55ca8d83d83cf0a95931f40ce8f6cee0c68570b6
GordonBreazz <jc12-mail@yandex.ru>
2020-06-18 22:37:56 +0800

    add API data and loader indicator

--- src/App.js
+++ src/App.js
@@ -1,46 +1,66 @@
-import React, { useState } from 'react';
-import TodoList from './todo/TodoList';
-import AddTodo from './todo/AddTodo';
-import './App.css';
-import Context from './context'
+import React, { useEffect } from "react"
+import TodoList from "./todo/TodoList"
+import AddTodo from "./todo/AddTodo"
+import "./App.css"
+import Context from "./context"
+import Loader from "./loader"
 
 function App() {
-  const [todos, setTodos] = React.useState(
-    [
-      {id: 1, copmleted: false, title: 'Купить хлеб'},
-      {id: 2, copmleted: true, title: 'Купить масло'},
-      {id: 3, copmleted: false, title: 'Купить молоко'}
-    ]    
-  )
+  const [todos, setTodos] = React.useState([])
+  const [loading, setLoading] = React.useState(true)
 
-  function toggleTodo(id){
-    setTodos(todos.map( todo => {
-      if (todo.id === id) todo.copmleted = !todo.copmleted
-      return todo
-    }))
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos?_limit=5")
+      .then((response) => response.json())
+      .then((todos) => {
+        setTimeout(() => {
+          setTodos(todos)
+          setLoading(false)
+        }, 2000)
+      })
+  }, [])
+
+  function toggleTodo(id) {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === id) todo.copmleted = !todo.copmleted
+        return todo
+      })
+    )
   }
 
   function removeTodo(id) {
-    setTodos(todos.filter(todo => todo.id !== id))
+    setTodos(todos.filter((todo) => todo.id !== id))
   }
 
-  function addTodo(title) {    
-    setTodos(todos.concat([{
-      title,
-      id: Date.now,
-      completed: false
-    }]))
+  function addTodo(title) {
+    setTodos(
+      todos.concat([
+        {
+          title,
+          id: Date.now(),
+          completed: false,
+        },
+      ])
+    )
   }
 
   return (
-    <Context.Provider value={{removeTodo}}>
-    <div className="wrapper">    
-      <h1>Руководство по реакт</h1>
-      <AddTodo onCreate={addTodo}/>
-      { todos.length ? (<TodoList todos = {todos} onToggle={toggleTodo}/>) : (<p>Нет задач</p>) } 
-    </div>
+    <Context.Provider value={{ removeTodo }}>
+      <div className="wrapper">
+        <h1>Руководство по реакт</h1>
+        <AddTodo onCreate={addTodo} />
+
+        {loading && <Loader/>}
+
+        {todos.length ? (
+          <TodoList todos={todos} onToggle={toggleTodo} />
+        ) : (
+          loading ? null : (<p>Нет задач</p>)
+        )}
+      </div>
     </Context.Provider>
   )
 }
 
-export default App;
+export default App
